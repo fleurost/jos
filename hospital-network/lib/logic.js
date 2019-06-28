83f312ee24a9bc8a2a9126a58d63155fa570dd2d
Your Name <you@example.com>
2019-06-28 11:15:46 +0700

    aw

--- hospital-network/lib/logic.js
+++ hospital-network/lib/logic.js
@@ -17,83 +17,27 @@
  * Write your transction processor functions here
  */
 
-/**
- * Update Data Medis
- * @param {org.hospital.record.UpdateDataMedis} tx
- * @transaction
- */
-
-/**
-async function UpdateDataMedis(tx) {
-
-    tx.datamedis.tglMasuk = tx.newTglMasuk;
-    tx.datamedis.tglKeluar = tx.newTglKeluar;
-    tx.datamedis.alasanMasuk = tx.newAlasanMasuk;
-    tx.datamedis.rujukan = tx.newRujukan;
-    tx.datamedis.anamnesis = tx.newAnamnesis;
-    tx.datamedis.pemeriksaanFisik = tx.newPemeriksaanFisik;
-    tx.datamedis.riwayatAlergi = tx.newRiwayatAlergi;
-    tx.datamedis.diagnosaPrimer = tx.newDiagnosaPrimer;
-    tx.datamedis.diagnosaSekunder = tx.newDiagnosaSekunder;
-    tx.datamedis.terapiDiRs = tx.newTerapiDiRs;
-    tx.datamedis.tindakan = tx.newTindakan;
-    tx.datamedis.prognosaPenyakit = tx.newPrognosaPenyakit;
-    tx.datamedis.alasanPulang = tx.newAlasanPulang;
-    tx.datamedis.kondisiSaatPulang = tx.newKondisiSaatPulang;
-    tx.datamedis.rencanaTindakLanjut = tx.newRencanaTindakLanjut;
-
-    return getAssetRegistry('org.hospital.record.DataMedis')
-        .then(function(assetRegistry){
-            return assetRegistry.update(tx.datamedis);
-        })
-        .catch(function(error){
-            console.log(error);
-        })
-}
-*/
-
-/**
- * Update Data Pasien
- * @param {org.hospital.record.UpdateDataPasien} tx
- * @transaction
- */
-
-/**
-async function UpdateDataPasien(tx) {
-
-    tx.pasien.namaLengkap = tx.newNamaLengkap;
-    tx.pasien.tempatTglLahir = tx.newTempatTglLahir;
-    tx.pasien.jenisKelamin = tx.newJenisKelamin;
-    tx.pasien.umur = tx.newUmur;
-    tx.pasien.alamat = tx.newAlamat;
-    tx.pasien.statusPernikahan = tx.newStatusPernikahan;
-    tx.pasien.goldar = tx.newGoldar;
-
-    return getAssetRegistry('org.hospital.record.Pasien')
-        .then(function(assetRegistry){
-            return assetRegistry.update(tx.pasien);
-        })
-        .catch(function(error){
-            console.log(error);
-        })
-}
-*/
 
 /**
  * Handle a transaction that returns a string.
  * @param {org.hospital.record.CheckIntegrity} transaction
  * @transaction
  */
-
 async function CheckIntegrity(transaction) {
-            return getAssetRegistry('org.hospital.record.DataMedis').then(function (dataMedisRegistry) {
-                return dataMedisRegistry.getAll().then(function (dataMedisList) {
+    var diagnosaPrimer = [];
+    return getAssetRegistry('org.hospital.record.DataMedis').then(function (assetRegistry) {
+        return assetRegistry.getAll().then(function(resourceCollection) {
+                resourceCollection.forEach(function (resource) {
+                    diagnosaPrimer.push(resource.diagnosaPrimer);
+
                     var factory = getFactory();
                     var newEvent = factory.newEvent('org.hospital.record', 'DataMedisSent');
-                    newEvent.dataMedisList = dataMedisList;
+                    newEvent.diagnosaPrimer = diagnosaPrimer;
                     emit(newEvent);
-                }).catch(function (error) {
-                    console.error(error);
                 });
-            });
+
+        }).catch(function(error){
+            console.log(error);
+        });
+    });
 }
--- joinChannel2.sh
+++ joinChannel2.sh
@@ -3,7 +3,6 @@
 
 docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.hospital.com/users/Admin@org1.hospital.com/msp"  peer0.org1.hospital.com peer channel create -o orderer.hospital.com:7050 -c rschannel -f /var/hyperledger/channel-artifacts/channel.tx --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hospital.com/msp/tlscacerts/tlsca.hospital.com-cert.pem
 
-      
 
 # Join peer0.org1.example.com to the channel.
 docker exec  -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.hospital.com/users/Admin@org1.hospital.com/msp"  peer0.org1.hospital.com peer channel join -b rschannel.block
